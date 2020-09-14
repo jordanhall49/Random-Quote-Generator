a5a2e3b6374d29871300db5ada263b4f22cca93a
Jordan Hall <jhall49@gmail.com>
2020-09-14 13:04:02 -0700

    fixing printQuote function

--- js/script.js
+++ js/script.js
@@ -11,19 +11,81 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-
+const quotes = [
+  {
+    quote: "Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.",
+    source: 'Michael Scott',
+    citation: 'The Office',
+    year: 2009,
+    season: 'Season 5, "The Duel"'
+  },
+  {
+    quote: "I talk a lot, so I’ve learned to tune myself out.",
+    source: 'Kelly Kapoor',
+    citation: 'The Office',
+    year: 2011,
+    season: 'Season 7, “Counseling”'
+  },
+  {
+    quote: "If I don’t have some cake soon, I might die.",
+    source: 'Stanley Hudson',
+    citation: 'The Office',
+    year: 2008,
+    season: 'Season 4, “Survivor Man”'
+  },
+  {
+    quote: 'Identity theft is not a joke, Jim! Millions of families suffer every year.',
+    source: 'Dwight Shrute',
+    citation: 'The Office',
+    year: 2007,
+    season: 'Season 3, “Product Recall”'
+  },
+  {
+    quote: "There’s a lot of beauty in ordinary things. Isn’t that kind of the point?",
+    source: 'Pam Beesly-Halpert',
+    citation: 'The Office',
+  }
+];
 
 /***
  * `getRandomQuote` function
 ***/
 
+//Generate random number from array index positions
 
+function getRandomQuote(){
+
+  let randomQuote = Math.floor( Math.random() * quotes.length);
+  return quotes[randomQuote];
+}
+
+// console.log(getRandomQuote(quotes));
 
 /***
  * `printQuote` function
 ***/
 
 
+function printQuote(){
+  const quoteObject = getRandomQuote();
+
+  let html = `<p class="quote"> ${quoteObject.quote} </p>`;
+  html += `<p class="source"> ${quoteObject.source}`;
+     
+  if (quoteObject.citation){
+      html += `<span class="citation"> ${quoteObject.citation}</span>`
+    };
+  if (quoteObject.year){
+      html += `<span class="year"> ${quoteObject.year}</span>`
+    };
+  if (quoteObject.season){
+    html += `<p class="season"> ${quoteObject.season}</span>`
+  };
+  html += '</p>'
+
+  document.getElementById('quote-box').innerHTML = html; 
+
+}
 
 /***
  * click event listener for the print quote button
